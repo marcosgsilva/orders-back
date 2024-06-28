5c1b85aa30f6b8c3a7565a1ba7c1ecdf1f5f86ba
marcos <marcospaulo.silva2009@hotmail.com>
2024-06-28 19:22:57 -0300

    refactor: ajusts of controller for searching

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 
 class OrderController extends Controller
 {
     public function index(Request $request)
     {
-        $orders  = Order::all();
-        return response()->json($request);
+        $query  = Order::query();
+
+        foreach ($request->all() as $key => $value) {
+            if (in_array($key, Schema::getColumnListing('orders')) && !empty($value)) {
+                $query->where($key, 'LIKE', "%{$value}%");
+            }
+        }
+
+        $orders = $query->get();
+
+        return response()->json($orders)
+            ->header('Cache-Control', 'no-cache,no-store,must-revalidate')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', '0');
     }
 
 
