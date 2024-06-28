7f0daa27cd4178827a3876e05b505e1f28788c85
marcos <marcospaulo.silva2009@hotmail.com>
2024-06-28 13:07:13 -0300

    refactor: create of new migration to update status size

--- database/seeders/OrdersTableSeeder.php
+++ database/seeders/OrdersTableSeeder.php
@@ -15,13 +15,29 @@ class OrdersTableSeeder extends Seeder
     {
         $numberOfOrders = 2000;
 
-        for($i=1; $i<=$numberOfOrders; $i++){
-            Order::create([
-                'status' => 'P', //pendente
-                'customer_name' => 'Customer '.$i,
-                'descriptiion' => 'Description for order '.$i,
-                'quantity' => rand(1,10),
-            ]);
+        for ($i = 1; $i <= $numberOfOrders; $i++) {
+            if ($i > 0 && $i <= 300) {
+                Order::create([
+                    'status' => 'SUCESSO', //pendente
+                    'customer_name' => 'Cliente ' . $i,
+                    'description' => 'Descrição do cliente ' . $i,
+                    'quantity' => rand(1, 10),
+                ]);
+            } else  if ($i > 300 && $i <= 400) {
+                Order::create([
+                    'status' => 'CANCELADO', //pendente
+                    'customer_name' => 'Cliente ' . $i,
+                    'description' => 'Descrição do cliente ' . $i,
+                    'quantity' => rand(1, 10),
+                ]);
+            } else  if ($i > 400) {
+                Order::create([
+                    'status' => 'PENDENTE', //pendente
+                    'customer_name' => 'Cliente ' . $i,
+                    'description' => 'Descrição do cliente  ' . $i,
+                    'quantity' => rand(1, 10),
+                ]);
+            }
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,23 +5,13 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-
-Route::get('/products', function(){
+Route::get('/products', function () {
     return Product::all();
 });
 
-Route::resource('orders', OrderController::class);
+Route::get('/orders', [OrderController::class, 'index']);
+Route::get('/orders/{id}', [OrderController::class, 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
